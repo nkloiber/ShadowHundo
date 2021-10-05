86529d0cbf36635518d3c7730a1ca584041c21a7
nkloiber <85385412+nkloiber@users.noreply.github.com>
2021-10-05 11:50:13 -0400

    changes

--- js/site.js
+++ js/site.js
@@ -1,21 +1,66 @@
 //displays a message to the user
-function displayMessage() {
-    //declare our variable
-    let msg = "";
+function displayHundo() {
 
-    //pull the value from the input box
-    msg = document.getElementById("message").value;
+    let startValue = 0;
+    let endValue = 100;
 
-    //alert the user
-    alert(msg);
-}
+    //get the numbers from the UI
+    startValue = document.getElementById("startValue").value;
+    endValue = document.getElementById("endValue").value;
+
+    //validate our numbers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    //check to see if they are numbers
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        //both numbers and integers
+
+        //display the numbers from start value to end value
+        let numbers = generateNumbers(startvalue, endValue);
+
+        displayNumbers(numbers)
+    } else {
+        //One or MORE are not integers
+    }
 
 
-function displayMessage2() {
+    //generate numbers from startvalue to endvalue
+    function generateNumbers(startValue, endValue) {
 
-    let msg = "";
+        //0,1,2,3,4...
+        let numbers = [];
 
-    msg = document.getElementById("message").value;
+        //loop over the numbers until we reach the end value
+        for (let index = startValue; index < endValue; index++) {
 
-    Swal.fire(msg);
-}
+            //add each number to the array
+            numbers.push(index);
+        }
+        //returns the array of numbers
+        return numbers;
+
+    }
+
+    //display the numbers to the page
+    function displayNumbers(numbers) {
+
+        //0 first indiex value
+        //99 last index value
+        //length = 100
+
+        // 0,1,2,3,4,5,6
+        let startValue = numbers[0];
+        let endIndex = numbers.length; //=100
+
+        let ptag = document.getElementById("results");
+
+        for (let index = 0; index < endIndex; index++) {
+
+            //get the actual number
+            let number = numbers[index];
+            ptag.innerHTML = number;
+
+        }
+
+    }
