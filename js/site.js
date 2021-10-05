264fca37f2684fc8d410d8dfdd450ab120595bbd
nkloiber <85385412+nkloiber@users.noreply.github.com>
2021-10-05 12:32:20 -0400

    changes

--- js/site.js
+++ js/site.js
@@ -17,50 +17,48 @@ function displayHundo() {
         //both numbers and integers
 
         //display the numbers from start value to end value
-        let numbers = generateNumbers(startvalue, endValue);
+        let numbers = generateNumbers(startValue, endValue);
 
         displayNumbers(numbers)
     } else {
         //One or MORE are not integers
     }
+}
 
+//generate numbers from startvalue to endvalue
+function generateNumbers(startValue, endValue) {
 
-    //generate numbers from startvalue to endvalue
-    function generateNumbers(startValue, endValue) {
+    //0,1,2,3,4...
+    let numbers = [];
 
-        //0,1,2,3,4...
-        let numbers = [];
+    //loop over the numbers until we reach the end value
+    for (let index = startValue; index < endValue; index++) {
 
-        //loop over the numbers until we reach the end value
-        for (let index = startValue; index < endValue; index++) {
-
-            //add each number to the array
-            numbers.push(index);
-        }
-        //returns the array of numbers
-        return numbers;
-
+        //add each number to the array
+        numbers.push(index);
     }
+    //returns the array of numbers
+    return numbers;
 
-    //display the numbers to the page
-    function displayNumbers(numbers) {
+}
 
-        //0 first indiex value
-        //99 last index value
-        //length = 100
+//display the numbers to the page
+function displayNumbers(numbers) {
 
-        // 0,1,2,3,4,5,6
-        let startValue = numbers[0];
-        let endIndex = numbers.length; //=100
+    //0 first indiex value
+    //99 last index value
+    //length = 100
 
-        let ptag = document.getElementById("results");
+    // 0,1,2,3,4,5,6
+    let startValue = numbers[0];
+    let endIndex = numbers.length; //=100
 
-        for (let index = 0; index < endIndex; index++) {
+    let ptag = document.getElementById("results");
 
-            //get the actual number
-            let number = numbers[index];
-            ptag.innerHTML = number;
+    for (let index = 0; index < endIndex; index++) {
 
-        }
-
+        //get the actual number
+        let number = numbers[index];
+        ptag.innerHTML += number + " | ";
     }
+}
